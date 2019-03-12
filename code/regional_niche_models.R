d1f7f11f339d7a2454efda24b60d6ab84d827273
David Ackerly <dackerly@berkeley.edu>
2019-03-11 22:23:05 -0700

    added WNA crop code to regional_niche_models.R

--- code/regional_niche_models.R
+++ code/regional_niche_models.R
@@ -19,6 +19,13 @@ clim <- list.files("big_data/climate", full.names=T) %>%
       raster()
 
 # clip climate data to study area
+WNAx <- extent(clim)
+WNAx@xmin <- -150
+WNAx@xmax <- -90
+WNAx@ymin <- 20
+WNAx@ymax <- 80
+clim <- crop(clim,WNAx)
+plot(clim)
 
 # sample background pseudoabsence data in propotion to spatial climate frequency
 
